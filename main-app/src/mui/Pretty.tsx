858ad20541e73e469e2e62a25333c11cdfae8c68
Craig Miller <craigmiller160@gmail.com>
2023-07-13 11:40:06 -0400

    Tree shaking is working

--- main-app/src/mui/Pretty.tsx
+++ main-app/src/mui/Pretty.tsx
@@ -1,7 +1,8 @@
-import {Button} from '@mui/material';
+import {Button, Typography} from '@mui/material';
 
 export const Pretty = () => (
     <div>
         <Button variant="contained">My Button</Button>
+        <Typography>Title</Typography>
     </div>
 )
