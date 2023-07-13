f50370de36b5d2011d90e8e982e93baf8184aa6e
Craig Miller <craigmiller160@gmail.com>
2023-07-13 11:42:44 -0400

    Progress

---
+++ mfe1/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+    root: path.join(process.cwd(), 'src'),
+    server: {
+        port: 3001
+    },
+    plugins: [
+        react()
+    ],
+    build: {
+        outDir: path.join(process.cwd(), 'build'),
+        emptyOutDir: true
+    }
+})
