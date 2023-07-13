8d4d2838387df402ced0a67a4367d3010b0a1d48
Craig Miller <craigmiller160@gmail.com>
2023-07-13 11:35:40 -0400

    Added material ui

--- main-app/src/App.tsx
+++ main-app/src/App.tsx
@@ -1,8 +1,10 @@
 import {Other} from './jsAndTs/Other';
+import {Pretty} from './mui/Pretty';
 
 export const App = () => (
     <div>
         <h1>Welcome to the Main App</h1>
         <Other />
+        <Pretty />
     </div>
 );
---
+++ main-app/src/mui/Pretty.tsx
@@ -0,0 +1,7 @@
+import {Button} from '@mui/material';
+
+export const Pretty = () => (
+    <div>
+        <Button variant="contained">My Button</Button>
+    </div>
+)
