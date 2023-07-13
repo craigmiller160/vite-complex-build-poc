ae82e4c3208299bcff146e911b057cc0f245f99b
Craig Miller <craigmiller160@gmail.com>
2023-07-13 13:53:52 -0400

    Cleanup

--- main-app/src/App.tsx
+++ main-app/src/App.tsx
@@ -1,10 +1,13 @@
 import {Other} from './jsAndTs/Other';
 import {Pretty} from './mui/Pretty';
+// @ts-ignore
+import { App as Mfe1App } from 'mfe1/App';
 
 export const App = () => (
     <div>
         <h1>Welcome to the Main App</h1>
         <Other />
         <Pretty />
+        <Mfe1App />
     </div>
 );
