e1b5309e3ba9906a6c79e2fa1156a91f18f335f2
Craig Miller <craigmiller160@gmail.com>
2023-07-14 15:37:39 -0400

    Finished new config

--- libs/config/vite.config.ts
+++ libs/config/vite.config.ts
@@ -1,10 +1,13 @@
-import {defineConfig, UserConfigExport} from 'vite';
+import {defineConfig, UserConfigExport, Plugin} from 'vite';
 import path from 'path';
+import fs from 'fs';
 import react from '@vitejs/plugin-react-swc';
 import topLevelAwait from 'vite-plugin-top-level-await';
-import type { Exposes, Shared, Remotes } from '@originjs/vite-plugin-federation'
-import packageJson from './package.json';
+import federation, { Exposes, Shared, Remotes } from '@originjs/vite-plugin-federation';
 
+type PackageJson = Readonly<{
+    name: string;
+}>;
 type NodeEnv = 'development' | 'test' | 'production';
 export type ViteConfig = Readonly<{
     port: number;
@@ -15,6 +18,22 @@ export type ViteConfig = Readonly<{
     }>;
 }>;
 
+const configureFederation = (config?: ViteConfig['federation']): Plugin | undefined => {
+    if (!config) {
+        return undefined;
+    }
+
+    const packageJson: PackageJson = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8'));
+
+    return federation({
+        name: packageJson.name,
+        filename: 'remoteEntry.js',
+        exposes: config.exposes,
+        remotes: config.remotes,
+        shared: config.shared
+    });
+}
+
 export const configureVite = (config: ViteConfig): UserConfigExport => {
     const nodeEnv: NodeEnv = process.env.NODE_ENV as NodeEnv;
     return defineConfig({
@@ -24,7 +43,8 @@ export const configureVite = (config: ViteConfig): UserConfigExport => {
         },
         plugins: [
             react(),
-            topLevelAwait()
+            topLevelAwait(),
+            configureFederation(config.federation)
         ],
         build: {
             outDir: path.join(process.cwd(), 'build'),
