4b4cf11c7232416f86ab730f541ec08bcccf1fe5
Craig Miller <craigmiller160@gmail.com>
2023-07-14 15:41:28 -0400

    Progress

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -1,26 +1,5 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import react from '@vitejs/plugin-react-swc';
-import federation from '@originjs/vite-plugin-federation';
-import topLevelAwait from 'vite-plugin-top-level-await';
+import { configureVite } from 'config/vite.config';
 
-export default defineConfig({
-    root: path.join(process.cwd(), 'src'),
-    server: {
-        port: 3000
-    },
-    plugins: [
-        react(),
-        topLevelAwait(),
-        federation({
-            name: 'main-app',
-            remotes: {
-                mfe1: 'http://localhost:3001/assets/remoteEntry.js'
-            }
-        })
-    ],
-    build: {
-        outDir: path.join(process.cwd(), 'build'),
-        emptyOutDir: true
-    }
-})
+configureVite({
+    port: 3000
+});
