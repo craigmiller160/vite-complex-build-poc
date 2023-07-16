a18c88b8a2ef8dc1e5088dacd371af2fd22d5f6c
Craig Miller <craigmiller160@gmail.com>
2023-07-16 15:01:49 -0400

    Fixing some config files

--- apps/mfe1/vite.config.ts
+++ apps/mfe1/vite.config.ts
@@ -1,14 +1,14 @@
 import packageJson from './package.json';
-import {configureViteApplication} from 'config';
+import { configureViteApplication } from 'config';
 
 export default configureViteApplication({
-    port: 3000,
-    federation: {
-        exposes: {
-            './App': './src/App.tsx'
-        },
-        shared: {
-            ...packageJson.dependencies
-        }
-    }
+	port: 3001,
+	federation: {
+		exposes: {
+			'./App': './src/App.tsx'
+		},
+		shared: {
+			...packageJson.dependencies
+		}
+	}
 });
