4c85cb543a56ff3a4e67d1a1c23ca62a3e591ce1
Craig Miller <craigmiller160@gmail.com>
2023-07-16 18:25:47 -0400

    Clenaup

--- apps/mfe1/vite.config.ts
+++ apps/mfe1/vite.config.ts
@@ -1,11 +1,57 @@
-import packageJson from './package.json';
-import { configureViteApplication } from 'config';
+import { defineConfig } from 'vite';
+import path from 'path';
+import fs from 'fs';
+import react from '@vitejs/plugin-react-swc';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import federation, { Exposes, Remotes } from '@originjs/vite-plugin-federation';
 
-export default configureViteApplication({
-	port: 3001,
-	federation: {
-		exposes: {
-			'./App': './src/App.tsx'
+type PackageJson = Readonly<{
+	name: string;
+	dependencies: Record<string, string>;
+}>;
+type NodeEnv = 'development' | 'test' | 'production';
+export type ViteAppConfig = Readonly<{
+	port: number;
+	federation?: Readonly<{
+		exposes?: Exposes;
+		remotes?: Remotes;
+	}>;
+}>;
+
+const srcDir = path.join(process.cwd(), 'src');
+const buildDir = path.join(process.cwd(), 'build');
+const packageJsonFile = path.join(process.cwd(), 'package.json');
+
+const nodeEnv: NodeEnv = process.env.NODE_ENV as NodeEnv;
+
+const packageJson: PackageJson = JSON.parse(
+	fs.readFileSync(packageJsonFile, 'utf8')
+);
+
+export default defineConfig({
+	root: srcDir,
+	server: {
+		port: 3001
+	},
+	plugins: [
+		react(),
+		topLevelAwait(),
+		federation({
+			name: packageJson.name,
+			exposes: {
+				'./App': './src/App.tsx'
+			},
+			shared: ['react']
+		})
+	],
+	build: {
+		outDir: buildDir,
+		emptyOutDir: true,
+		minify: nodeEnv === 'production' ? 'terser' : false,
+		cssMinify: nodeEnv === 'production' ? undefined : false,
+		cssCodeSplit: nodeEnv === 'production',
+		rollupOptions: {
+			treeshake: nodeEnv === 'production' ? 'recommended' : false
 		}
 	}
 });
