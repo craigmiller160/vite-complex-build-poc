20bf3442cae441ede274eea2b3fed8975b2dcc17
Craig Miller <craigmiller160@gmail.com>
2023-07-16 18:51:57 -0400

    Got lib to work

--- libs/config/vite.config.ts
+++ libs/config/vite.config.ts
@@ -3,44 +3,43 @@ import path from 'path';
 import packageJson from './package.json';
 import dts from 'vite-plugin-dts';
 import { builtinModules } from 'module';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
-const dependencies = Object.entries(packageJson.dependencies)
-    .map(([key]) => key);
+const dependencies = [
+	...Object.keys(packageJson.dependencies),
+	...Object.keys(packageJson.devDependencies)
+];
 
 const rootDir = path.join(process.cwd(), 'src');
 const buildDir = path.join(process.cwd(), 'build');
 
 export default defineConfig({
-    plugins: [
-        dts({
-            entryRoot: rootDir,
-            insertTypesEntry: true
-        }),
-        topLevelAwait(),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: path.join(process.cwd(), 'tsconfig.base.json'),
-                    dest: buildDir
-                }
-            ]
-        })
-    ],
-    build: {
-        outDir: buildDir,
-        emptyOutDir: true,
-        lib: {
-            entry: rootDir,
-            name: 'config',
-            fileName: 'config'
-        },
-        rollupOptions: {
-            external: [
-                ...dependencies,
-                ...builtinModules
-            ]
-        }
-    }
+	plugins: [
+		dts({
+			entryRoot: rootDir,
+			insertTypesEntry: true
+		}),
+		topLevelAwait(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: path.join(process.cwd(), 'tsconfig.base.json'),
+					dest: buildDir
+				}
+			]
+		})
+	],
+	build: {
+		outDir: buildDir,
+		emptyOutDir: true,
+		lib: {
+			entry: rootDir,
+			name: 'config',
+			fileName: 'config'
+		},
+		rollupOptions: {
+			external: [...dependencies, ...builtinModules]
+		}
+	}
 });
