3348a9409f6985c10116658d80d3c13700f269d5
Craig Miller <craigmiller160@gmail.com>
2023-07-17 16:41:19 -0400

    Adding toggle to show MFE1

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,14 +1,25 @@
 import { Other } from './jsAndTs/Other';
 import { Pretty } from './mui/Pretty';
 import { App as Mfe1App } from 'mfe1/App';
-import { CircularProgress } from '@mui/material';
+import { Button, CircularProgress } from '@mui/material';
 import { LazyWrapper } from 'react-lazy';
+import { useState } from 'react';
 
-export const App = () => (
-	<div>
-		<h1>Welcome to the Main App</h1>
-		<Other />
-		<Pretty />
-		<LazyWrapper path="mfe1/App" loading={<CircularProgress />} />
-	</div>
-);
+export const App = () => {
+	const [showMfe1, setShowMfe1] = useState<boolean>(false);
+	const toggleShowMfe1 = () => setShowMfe1((prevState) => !prevState);
+
+	return (
+		<div>
+			<h1>Welcome to the Main App</h1>
+			<Other />
+			<Pretty />
+			<Button variant="contained" onClick={toggleShowMfe1}>
+				Toggle MFE1
+			</Button>
+			{showMfe1 && (
+				<LazyWrapper path="mfe1/App" loading={<CircularProgress />} />
+			)}
+		</div>
+	);
+};
