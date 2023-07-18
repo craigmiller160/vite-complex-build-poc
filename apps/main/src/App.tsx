2f0b798bead6989ae9749db130eaad29396b6f59
Craig Miller <craigmiller160@gmail.com>
2023-07-18 14:55:40 -0400

    Progress with refactor

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,12 +1,29 @@
 import { Other } from './jsAndTs/Other';
 import { Pretty } from './mui/Pretty';
 import { App as Mfe1App } from 'mfe1/App';
+import { useState } from 'react';
+import { Button } from '@mui/material';
 
-export const App = () => (
-	<div>
-		<h1>Welcome to the Main App</h1>
-		<Other />
-		<Pretty />
-		<Mfe1App />
-	</div>
-);
+import('mfe1/App').then((res) => {
+	console.log('RES', res.default.App);
+});
+
+export const App = () => {
+	const [showMfe1, setShowMfe1] = useState<boolean>(false);
+	const toggleShowMfe1 = () => setShowMfe1((prevState) => !prevState);
+	return (
+		<div>
+			<h1>Welcome to the Main App</h1>
+			<Other />
+			<Pretty />
+			<Button variant="contained" onClick={toggleShowMfe1}>
+				Toggle MFE1
+			</Button>
+			{
+				showMfe1 &&
+				<Mfe1App />
+			}
+
+		</div>
+	);
+};
